bd6c521a14e64d5131af7f35ac0512befa4eca5f
tobi994 <viettt.fuky@gmail.com>
2018-06-17 20:49:19 +0700

    animtion

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,6 +15,7 @@ public class PlayerController : MonoBehaviour
 
     //Variables of types Rigidbody and Animator
     private Rigidbody2D RBody;
+    [SerializeField]
     private Animator Anim;
 
     //Variable for Player movement speed
@@ -22,7 +23,9 @@ public class PlayerController : MonoBehaviour
     public float playerSpeed = 0;
     public PlayerManager playerManager;
     [SerializeField]
-    private Vector2 movementVector;
+    private Vector2 movementVector = Vector2.zero;
+    [SerializeField]
+    private Vector2 lastMovementVector = Vector2.zero;
 
     void Start()
     {
@@ -31,43 +34,45 @@ public class PlayerController : MonoBehaviour
 
         // set initial speed to default and current speed to default
         playerSpeed = playerManager.defaultMoveSpeed * Time.deltaTime;
-        playerManager.currentMoveSpeed = playerManager.defaultMoveSpeed + 1f;
+        playerManager.currentMoveSpeed = playerManager.defaultMoveSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Gets Player input. GetAxisRaw returns true or false, GetAxis allows floating point precision which we dont need for this level of movement
+        movementVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        movementVector.Normalize(); // magnitude set at 1 always so diagonal movement isn't faster
+        playerManager.canMove = (movementVector != Vector2.zero);
 
+        this.Anim.SetBool("isMove", playerManager.canMove);
         if (playerManager.canMove)
         {
-            //Gets Player input. GetAxisRaw returns true or false, GetAxis allows floating point precision which we dont need for this level of movement
-            movementVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-            movementVector.Normalize(); // magnitude set at 1 always so diagonal movement isn't faster
+            lastMovementVector = movementVector;
             // set updated speed to currentSpeed
             playerSpeed = playerManager.currentMoveSpeed * Time.deltaTime;
 
-            if (Anim)
-            {
-                
-                //Checks to see if movement vector is equal to zero. If not zero, means we set the animator to walking else dont set to walking
-                if (movementVector != Vector2.zero)
-                {
-                    Anim.SetBool("isWalking", true);
+            //Updates the direction so that we don't snap back to original position 
+            Anim.SetFloat("Velocity_X", movementVector.x);
+            Anim.SetFloat("Velocity_Y", movementVector.y);
 
-                    //Updates the direction so that we don't snap back to original position 
-                    Anim.SetFloat("input_x", movementVector.x);
-                    Anim.SetFloat("input_y", movementVector.y);
-                    Anim.SetFloat("lastMove_x", movementVector.x);
-                    Anim.SetFloat("lastMove_y", movementVector.y);
-                }
-                else
-                {
-                    Anim.SetBool("isWalking", false);
-                }
-
-            }
+//            //Checks to see if movement vector is equal to zero. If not zero, means we set the animator to walking else dont set to walking
+//            if (movementVector != Vector2.zero)
+//            {
+//                Anim.SetBool("isWalking", true);
+//            }
+//            else
+//            {
+//                Anim.SetBool("isWalking", false);
+//            }
+                    
         }
-
+        else
+        {
+            Anim.SetFloat("Last_Velo_X", lastMovementVector.x);
+            Anim.SetFloat("Last_Velo_Y", lastMovementVector.y);
+        }
+         
     }
 
     void FixedUpdate()
