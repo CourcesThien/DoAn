687582eced2260d3c3370bb8fb65b8590edfdf98
tobi994 <viettt.fuky@gmail.com>
2018-06-18 00:40:10 +0700

    ok

---
+++ tech/Assets/Scripts/BoxKey.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxKey : MonoBehaviour
+{
+    public GameObject objDone;
+    private bool hasKey = false;
+
+    private Transform currTransKey = null;
+    // Use this for initialization
+    void Start()
+    {
+        objDone.SetActive(false);
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+        if (hasKey && currTransKey != null)
+        {
+            
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("BoxKey"))
+        {
+            hasKey = true;
+            currTransKey = other.transform;
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag.Equals("BoxKey"))
+        {
+            hasKey = true;
+            currTransKey = other.transform;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag.Equals("BoxKey"))
+        {
+            hasKey = false;
+        }
+    }
+
+}
---
+++ tech/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace RPG
+{
+    public class CameraFollow : MonoBehaviour
+    {
+
+        //References the Player object
+        public Transform Target;
+        private Vector3 posTarget = Vector3.zero;
+        public float camSpeed = 0.1f;
+        private float stepMove = 0;
+        private Transform myTranform;
+        // Use this for initialization
+        void Start()
+        {
+            myTranform = transform;
+        }
+
+        // LateUpdate is called after updating each frame
+        void Update()
+        {
+            if (Target)
+            {
+                stepMove = camSpeed * Time.deltaTime;
+                posTarget = Target.position;
+                posTarget.y = 0;
+                myTranform.position = Vector3.MoveTowards(myTranform.position, posTarget, stepMove);// Vector3.Lerp(myTranform.position, posTarget, camSpeed);
+            }
+        }
+    }
+}
